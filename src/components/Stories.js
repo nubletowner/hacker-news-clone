acef723390f6fa23e0088d3ca79669891bc204ff
Dennis Durairaj <dennis.durairaj@gmail.com>
2019-01-06 12:37:43 +0100

    Basic components with sample data

---
+++ src/components/Stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Stories.css';
+import Story from './Story';
+
+const Stories = ({ stories }) => {
+  return (
+    <div className="stories">
+      {stories.map(story => {
+        return <Story story={story} key={story.objectID} />;
+      })}
+    </div>
+  );
+};
+
+export default Stories;
